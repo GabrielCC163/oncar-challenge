1f76d5f0605fda9f0d5bea24be0fe50db13f5076
Gabriel Brum <gabriel.brum@dotgroup.com.br>
2020-08-04 22:11:45 -0300

    finished

---
+++ README.md
@@ -0,0 +1,53 @@
+# OnCar - Full Stack Node.js Challenge
+
+### Requisitos para acesso local:
+Node.js, Yarn, Live Server e Docker
+
+### Siga os passos abaixo:
+
+* #### Crie uma pasta para armazenamento do banco de dados:
+  
+    ```
+    $ sudo mkdir -p /storage/docker/mysql-data
+    ```
+
+* #### Clone o projeto:
+    ```
+    $ git clone https://github.com/GabrielCC163/oncar-challenge.git
+    ```
+* #### Instale as dependências da API e inicialize o banco de dados (MySQL):
+    ```
+    $ cd oncar-challenge/api && yarn install
+
+    $ mv .env_sample .env
+
+    $ yarn mysql
+    ```
+* #### Crie a base de dados 'oncar' e execute a migration:
+    ```
+    $ docker exec -it docker-oncar bash
+
+    $ mysql -p
+
+    digite password (especificamente)
+
+    $ create database oncar;
+
+    encerre o acesso do mysql e do container (exit; + Enter)
+
+    $ cd oncar-challenge/api && npx knex migrate:latest
+    ```
+
+* #### Inicialize a API:
+    ```
+    $ cd oncar-challenge/api && yarn start
+    ```
+
+* #### Acesse a aplicação web:
+    ```
+    $ cd oncar-challenge/frontend && live-server .
+    ```
+
+### Importe as requisições e as execute via Insomnia:
+
+[![Run in Insomnia}](https://insomnia.rest/images/run.svg)](https://insomnia.rest/run/?label=OnCar%20Challenge%20API&uri=https%3A%2F%2Fgist.githubusercontent.com%2FGabrielCC163%2Fc4c94fbef5fb0d3637fb3483345493f9%2Fraw%2F93fa1c0bb5a2a23cf1fc9bca47c212bfb0fe1752%2Foncar_requests.json)
--- frontend/scripts/index.js
+++ frontend/scripts/index.js
@@ -2,7 +2,20 @@ let tabVehicles,
 	tabDetail,
 	defaultDetail,
 	searchField,
-	searchButton = null;
+	searchButton,
+	newVehicle,
+	submitNew,
+	pgInitial,
+	pgLeft,
+	pgRight,
+	pgEnd,
+	modalAction,
+	btnClose = null;
+
+let listVehicles = [];
+let indexOfLast = 0;
+let indexOfFirst = 0;
+let lastPage = 0;
 
 let allVehicles,
 	newVehicles = [];
@@ -18,6 +31,23 @@ window.addEventListener('load', () => {
 	defaultDetail = document.querySelector('#defaultDetail');
 	searchField = document.querySelector('#searchField');
 	searchButton = document.querySelector('#searchButton');
+	newVehicle = document.querySelector('#newVehicle');
+	submitNew = document.querySelector('#submitNew');
+	pgInitial = document.querySelector('#pgInitial');
+	pgLeft = document.querySelector('#pgLeft');
+	pgRight = document.querySelector('#pgRight');
+	pgEnd = document.querySelector('#pgEnd');
+	modalAction = document.querySelector('#modalAction');
+	btnClose = document.querySelector('#btnClose');
+
+	btnClose.addEventListener('click', () => {
+		modalAction.style.display = 'none';
+	});
+
+	submitNew.addEventListener('click', (event) => {
+		event.preventDefault();
+		handleSubmit();
+	});
 
 	fetchVehicles();
 	handleSearch();
@@ -77,11 +107,13 @@ function render() {
 }
 
 function renderVehiclesList() {
-	let listVehicles = newVehicles.length > 0 ? newVehicles : allVehicles;
-	const indexOfLast = page * perPage;
-	const indexOfFirst = indexOfLast - perPage;
-	currentVehicles = listVehicles.slice(indexOfFirst, indexOfLast);
-	const lastPage = Math.ceil(listVehicles.length / perPage);
+	listVehicles = newVehicles.length > 0 ? newVehicles : allVehicles;
+	indexOfLast = page * perPage;
+	indexOfFirst = indexOfLast - perPage;
+	currentVehicles = Array.from(listVehicles.slice(indexOfFirst, indexOfLast));
+	lastPage = Math.ceil(listVehicles.length / perPage);
+
+	handlePaginate(page, lastPage);
 
 	let vehiclesHTML = `
         <h5>Lista de veículos</h5>
@@ -93,10 +125,10 @@ function renderVehiclesList() {
 
 		const vehicleHTML = `
             <div id='${id}' class='vehicle'>
-                <div className="vehicle_info">
-                    <span>${marca}</span>
-                    <span>${veiculo}</span>
-                    <span>${ano}</span>
+                <div class="vehicle_info">
+                    <span class="vehicle_info__marca">${marca}</span>
+                    <span class="vehicle_info__veiculo">${veiculo}</span>
+                    <span class="vehicle_info__ano">${ano}</span>
                 </div>
                 <button type="button" class="btn btn-outline-secondary" aria-label="Editar">
                     <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-pencil-square" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
@@ -116,6 +148,7 @@ function renderVehiclesList() {
 	let vehiclesList = document.querySelectorAll('.vehicle');
 	vehiclesList.forEach((el) => {
 		el.addEventListener('click', () => renderVehicleDetail(el.id));
+		el.children[1].addEventListener('click', () => openModal(el.id));
 	});
 }
 
@@ -128,28 +161,144 @@ function renderVehicleDetail(id) {
 	let detailsHTML = `
         <h5>Detalhes do veículo</h5>
         <div class='details'>
-            <span class='vehicle_details_name'>${selectedVehicle.veiculo}</span>
+            <span class='vehicle_details__name'>${selectedVehicle.veiculo}</span>
             <div class='vehicle_details_group'>
-                <div class='vehicle_details_brand'>
+                <div class='vehicle_details__brand'>
                     <label>MARCA</label>
                     <span>${selectedVehicle.marca}</span>
                 </div>
-                <div class='vehicle_details_year'>
+                <div class='vehicle_details__year'>
                     <label>ANO</label>
                     <span>${selectedVehicle.ano}</span>
                 </div>
             </div>
-            <div class='vehicle_details_detalhes'>${selectedVehicle.descricao}</div>
-            <div class='edition'>
-                <button class='btn btn-outline-primary' onClick={openModal}>EDITAR</button>
-            </div>
-        </div>
+            <div class='vehicle_details__description'>${selectedVehicle.descricao}</div>
+		</div>
+		<div class='edition'>
+			<button id='edit${selectedVehicle.id}' class='btn btn-outline-primary'>EDITAR</button>
+		</div>
     `;
 
 	tabDetail.innerHTML = detailsHTML;
-	tabDetail.style.display = 'block';
+	tabDetail.style.display = 'flex';
+
+	document.querySelector(`#edit${id}`).addEventListener('click', () => openModal(id));
 }
 
 function setPage(newPage) {
 	page = newPage;
+	render();
+}
+
+function handlePaginate(pg, ltPage) {
+	pgInitial.addEventListener('click', () => {
+		setPage(1);
+	});
+
+	pgLeft.addEventListener('click', () => {
+		if (page === 1) {
+			return;
+		}
+		setPage(page - 1);
+	});
+
+	pgRight.addEventListener('click', () => {
+		if (page >= ltPage) {
+			return;
+		}
+		setPage(page + 1);
+	});
+
+	pgEnd.addEventListener('click', () => {
+		if (page >= ltPage) {
+			return;
+		}
+		setPage(ltPage);
+	});
+}
+
+async function openModal(search = '') {
+	if (search) {
+		const result = await fetch(`http://localhost:3333/veiculos/${search}`);
+		const resultJson = await result.json();
+
+		const { id, marca, descricao, veiculo, ano, vendido } = resultJson[0];
+
+		modalAction.style.display = 'block';
+		document.querySelector('#veiculo_id').value = id;
+		document.querySelector('#veiculo').value = veiculo;
+		document.querySelector('#marca').value = marca;
+		document.querySelector('#ano').value = ano;
+		document.querySelector('#descricao').value = descricao;
+		document.querySelector('#vendido').checked = vendido;
+	} else {
+		document.querySelector('#veiculo_id').value = '';
+		document.querySelector('#veiculo').value = '';
+		document.querySelector('#marca').value = '';
+		document.querySelector('#ano').value = '';
+		document.querySelector('#descricao').value = '';
+		document.querySelector('#vendido').checked = false;
+	}
+
+	modalAction.style.display = 'block';
+}
+
+window.onclick = function(event) {
+	if (event.target == modalAction) {
+		modalAction.style.display = 'none';
+	}
+};
+
+async function handleSubmit() {
+	let idVeiculo = document.querySelector('#veiculo_id').value;
+	let veiculo = document.querySelector('#veiculo').value;
+	let marca = document.querySelector('#marca').value;
+	let ano = document.querySelector('#ano').value;
+	let descricao = document.querySelector('#descricao').value;
+	let vendido = document.querySelector('#vendido').checked;
+
+	let res = null;
+	if (idVeiculo) {
+		res = await fetch(`http://localhost:3333/veiculos/${idVeiculo}`, {
+			method: 'PUT',
+			headers: new Headers({
+				'Content-Type': 'application/json'
+			}),
+			redirect: 'follow',
+			body: JSON.stringify({
+				veiculo,
+				marca,
+				ano,
+				descricao,
+				vendido
+			})
+		});
+	} else {
+		res = await fetch('http://localhost:3333/veiculos', {
+			method: 'POST',
+			headers: new Headers({
+				'Content-Type': 'application/json'
+			}),
+			redirect: 'follow',
+			body: JSON.stringify({
+				veiculo,
+				marca,
+				ano,
+				descricao,
+				vendido
+			})
+		});
+	}
+
+	if (res.status === 200) {
+		if (idVeiculo) {
+			alert('Alteração realizada com sucesso!');
+		} else {
+			alert('Veículo cadastrado com sucesso!');
+		}
+	}
+
+	modalAction.style.display = 'none';
+	tabDetail.style.display = 'none';
+	fetchVehicles();
 }
